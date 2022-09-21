2dd5bde02eb5ab4b4d58f3843143d44ea6023e7f
sm185592 <sm185592@ncr.com>
2022-09-21 13:44:15 +0100

    Altered the blue used by powershell, added a few more programs

--- tmux-dev-icons.py
+++ tmux-dev-icons.py
@@ -7,6 +7,9 @@ icons = {
     "python3" : "",
     "pwsh" : "",
     "zsh " : "",
+    "bash" : "",
+    "git" : "",
+    "fzf" : "",
 }
 
 colours = {
@@ -14,8 +17,11 @@ colours = {
     "vim" : "#078a2a",
     "python" : "#4b8bbe",
     "Python3" : "#4b8bbe",
-    "pwsh" : "#4568a9",
+    "pwsh" : "#5da4b5",
     "zsh" : "#eb5620",
+    "bash" : "#4ba244",
+    "git" : "#e14528",
+    "fzf" : "#ee0058"
 }
 
 pane_title = sys.argv[1]
